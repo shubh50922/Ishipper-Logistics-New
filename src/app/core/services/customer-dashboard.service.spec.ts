d96071f9684e53fed694da0a1118f2ecab4f4e63
shubh50922 <shubhanshi.a@cisinlabs.com>
2024-07-19 18:24:27 +0530

    commision form data fetched

---
+++ src/app/core/services/customer-dashboard.service.spec.ts
@@ -0,0 +1,15 @@
+import { TestBed } from '@angular/core/testing';
+
+import { CustomerDashboardService } from './customer-dashboard.service';
+describe('CustomerDashboardService', () => {
+  let service: CustomerDashboardService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(CustomerDashboardService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
---
+++ src/app/core/services/customer-dashboard.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CustomerDashboardService {
+  encrypt(txt: any): string {
+    return CryptoJS.AES.encrypt(txt, 'quotes').toString();
+  }
+  decrypt(txtToDecrypt: string) {
+    return CryptoJS.AES.decrypt(txtToDecrypt, 'quotes').toString(
+      CryptoJS.enc.Utf8
+    );
+  }
+
+  constructor() { }
+}
